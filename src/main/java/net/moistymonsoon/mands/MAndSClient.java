6ce821b66b83651715e9d4b9f54380ca5a45d6c2
MoistyMonsoonMods <monsoon360@outlook.com>
2024-06-24 02:48:02 +0100

    Initial

---
+++ src/main/java/net/moistymonsoon/mands/MAndSClient.java
@@ -0,0 +1,9 @@
+package net.moistymonsoon.mands;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MAndSClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
